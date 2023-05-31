61c1c0191a1ddadf918ceb768be3213161c164b4
Eber <doneber.dev@gmail.com>
2023-05-31 09:07:26 -0400

    Renderiza lista de datos

--- ejercicio02-HolaReactVite/src/App.jsx
+++ ejercicio02-HolaReactVite/src/App.jsx
@@ -6,21 +6,40 @@ const MySuperButton = () => {
   </button>
 }
 
-const user = {
-  name: "done",
-  nickname: "doneber",
-  githubURL: "https://github.com/doneber"
-}
+const userList = [
+  {
+    id: 1,
+    name: "done",
+    nickname: "doneber",
+    githubURL: "https://github.com/doneber"
+  },
+  {
+    id: 2,
+    name: "Linus",
+    nickname: "torvalds",
+    githubURL: "https://github.com/torvalds"
+  },
+  {
+    id: 3,
+    name: "Richard Stallman",
+    nickname: "stallmandev",
+    githubURL: "https://github.com/stallmandevUwU"
+  },
+]
 
 function App() {
   return (
     <>
       <h2>Usuario</h2>
-      <ul>
-        <li><b>Nombre: </b> { user.name }</li>
-        <li><b>Nickname: </b>{ user.nickname }</li>
-        <li><b>Enlace de GitHub: </b> <a href={ user.githubURL }>{ user.githubURL }</a> </li>        
-      </ul>
+      {
+        userList.map(user =>
+        <ul key={ user.id }>
+          <li><b>Nombre: </b> { user.name }</li>
+          <li><b>Nickname: </b>{ user.nickname }</li>
+          <li><b>Enlace de GitHub: </b> <a href={ user.githubURL }>{ user.githubURL }</a> </li>        
+        </ul>
+        )
+      }
       <MySuperButton />
     </>
   )
